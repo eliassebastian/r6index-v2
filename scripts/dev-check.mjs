548e4eee6237eef005b4c1e43b20b75143225ff0
Elias <eeschmoelz@gmail.com>
2024-02-29 20:58:30 +0000

    Refactor dev-check script to use 'pnpm run db:status' command

--- scripts/dev-check.mjs
+++ scripts/dev-check.mjs
@@ -1,18 +1,9 @@
 import { exec } from 'node:child_process';
 
-const containerName = 'supabase_db_r6index'; // Replace with your Supabase container name
-
 function checkContainerStatus() {
-	const command = `docker ps --filter "status=running" --filter "name=^${containerName}$"`;
-
-	exec(command, (error, stdout) => {
-		if (error) {
-			console.error(`Error: ${error.message}`);
-			process.exit(1);
-		}
-
-		if (stdout.includes(containerName)) {
-			console.log(`The Supabase container '${containerName}' is running.`);
+	exec('pnpm run db:status', (error, stdout) => {
+		if (stdout.includes('DB URL')) {
+			console.log('Supabase running.');
 		} else {
 			console.log('Supabase is not running. Please start it before proceeding.');
 			process.exit(1);
