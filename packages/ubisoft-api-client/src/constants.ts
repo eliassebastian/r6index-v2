29f301203f5d6be267721747c866d372ce3091a0
Elias <eeschmoelz@gmail.com>
2024-03-12 18:30:30 +0000

    new season Y9S1 constants

--- packages/ubisoft-api-client/src/constants.ts
+++ packages/ubisoft-api-client/src/constants.ts
@@ -1,3 +1,3 @@
-export const SEASON_NAME = 'Deep Freeze';
-export const SEASON_CODE = 'Y8S4';
-export const SEASON_START = 20231206;
+export const SEASON_NAME = 'Deadly Omen';
+export const SEASON_CODE = 'Y9S1';
+export const SEASON_START = 20240312;
